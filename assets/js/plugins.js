46484a8107ec60f02d13b650388c7d0cdbb6fb70
Andy Lulham <a.lulham@gmail.com>
2019-01-23 20:31:33 +0000

    es6 linting

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -1,6 +1,6 @@
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
 
-$(function () {
+$(() => {
   $('#collapseTwo').attr('class', 'panel-collapse collapse');
   $('#collapseThree').attr('class', 'panel-collapse collapse');
 });
