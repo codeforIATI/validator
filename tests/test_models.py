172af48a487fc1995a866894b6edf05efd6e1899
Andy Lulham <a.lulham@gmail.com>
2019-01-23 22:50:54 +0000

    Add a model test

---
+++ tests/test_models.py
@@ -0,0 +1,19 @@
+"""Model unit tests."""
+import pytest
+
+from iati_validator.public.models import SuppliedData
+from iati_validator.extensions import db
+
+
+@pytest.mark.usefixtures('db')
+class TestSuppliedData:
+    """SuppliedData tests."""
+
+    def test_get_by_uuid(self):
+        """Get SuppliedData by UUID."""
+        supplied_data = SuppliedData(None, None, 'Raw XML', 'text_form')
+        db.session.add(supplied_data)
+        db.session.commit()
+
+        retrieved = SuppliedData.query.get_or_404(supplied_data.id)
+        assert retrieved == supplied_data
